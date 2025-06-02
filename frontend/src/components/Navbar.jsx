c44f5acf451e2a57e671b880935977b8f330fcc5
GhugareSanika <144719999+GhugareSanika@users.noreply.github.com>
2025-06-02 13:36:14 +0530

    done

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -14,15 +14,15 @@ const Navbar = () => {
         <div className="flex flex-wrap justify-between items-center">
           <Link
             to="/"
-            className="text-2xl font-bold text-emerald-400 items-center space-x-2 flex"
+            className="text-3xl font-bold text-fuchsia-700 items-center space-x-2 flex"
           >
-            E-Commerce
+            TrendWear
           </Link>
 
           <nav className="flex flex-wrap items-center gap-4">
             <Link
               to={"/"}
-              className="text-gray-300 hover:text-emerald-400 transition duration-300
+              className="text-gray-300 hover:text-fuchsia-700 transition duration-300
 					 ease-in-out"
             >
               Home
@@ -30,17 +30,17 @@ const Navbar = () => {
             {user && (
               <Link
                 to={"/cart"}
-                className="relative group text-gray-300 hover:text-emerald-400 transition duration-300 
+                className="relative group text-gray-300 hover:text-fuchsia-700 transition duration-300 
 							ease-in-out"
               >
                 <ShoppingCart
-                  className="inline-block mr-1 group-hover:text-emerald-400"
+                  className="inline-block mr-1 group-hover:text-fuchsia-700"
                   size={20}
                 />
                 <span className="hidden sm:inline">Cart</span>
                 {cart.length > 0 && (
                   <span
-                    className="absolute -top-2 -left-2 bg-emerald-500 text-white rounded-full px-2 py-0.5 
+                    className="absolute -top-2 -left-2 bg-fuchsia-900 text-white rounded-full px-2 py-0.5 
 									text-xs group-hover:bg-emerald-400 transition duration-300 ease-in-out"
                   >
                     {cart.length}
@@ -50,7 +50,7 @@ const Navbar = () => {
             )}
             {isAdmin && (
               <Link
-                className="bg-emerald-700 hover:bg-emerald-600 text-white px-3 py-1 rounded-md font-medium
+                className="bg-fuchsia-700 hover:bg-fuchsia-700 text-white px-3 py-1 rounded-md font-medium
 								 transition duration-300 ease-in-out flex items-center"
                 to={"/secret-dashboard"}
               >
@@ -72,7 +72,7 @@ const Navbar = () => {
               <>
                 <Link
                   to={"/signup"}
-                  className="bg-emerald-600 hover:bg-emerald-700 text-white py-2 px-4 
+                  className="bg-fuchsia-700 hover:bg-fuchsia-700 text-white py-2 px-4 
 									rounded-md flex items-center transition duration-300 ease-in-out"
                 >
                   <UserPlus className="mr-2" size={18} />
